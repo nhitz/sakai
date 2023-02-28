a0d3faefabd39da23759431e820fea9d8a01230a
Neal <nealhitzfield@gmail.com>
2023-02-28 14:19:14 -0800

    fix vite build base URL

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig(({ command }) => {
     return {
         plugins: [vue()],
-        base: command === 'serve' ? '' : '/sakai-vue/',
+        base: command === 'serve' ? '' : '/',
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
